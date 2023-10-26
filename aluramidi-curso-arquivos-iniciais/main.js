c85815a22b5606e291f4f1adb61cf5021077ee55
niih19 <106820986+niih19@users.noreply.github.com>
2023-10-26 11:15:03 -0300

    Update main.js

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,12 +1,36 @@
 function tocaSom (seletorAudio) {
-    const elemento =document.querySelector(selectorAudio);
+    const elemento = document.querySelector (seletrAudio) ;
 
-    if (elemento && elemento.LocalName === 'audio') {
+    if (elemento && elemento.localName === 'audio') { 
         elemento.play();
     }
     else {
-        
-        console.log('Elemento não encontrado ou seletor inválido')
+        //alert('Elemento não encontrado');
+        console.long(Elemento não emcontrado ou seletor inálido);
+    }
+}
+
+const listaDeTeclas =document.querySelectorAll ('.tecla');
+
+//para
+for (let contador = 0 < listaDeTeclas.length; contador++){
+
+    const tecla = lista DeTeclas [contador];
+    const intrumento = tecla.classList[1];
+    const idAudio = 'som_${instrumento}'; //template string
+
+    tecla.onclik = function () {
+        tocaSo(idAudio);
     }
 
+    tecla.onkeydown = functiomn (evento) {
+
+        if (evento.code === 'Space'||evento.code === 'Enteder') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
 }
